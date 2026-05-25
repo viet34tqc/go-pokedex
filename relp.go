9a660f7b896c256b544a3dd2dcda6724e21fe33d
viet34tqc <viet34tqc@gmail.com>
2026-05-25 23:58:34 +0700

    Using arrow to navigate commands

--- relp.go
+++ relp.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"go-http-client/internal/pokeapi"
-	"os"
 	"strings"
+
+	"github.com/eiannone/keyboard"
 )
 
 type Config struct {
@@ -19,6 +19,32 @@ type cliCommand struct {
 	name        string
 	description string
 	callback    func(*Config, ...string) error
+}
+
+type commandHistory struct {
+	entries []string
+	limit   int
+}
+
+func newCommandHistory(limit int) *commandHistory {
+	return &commandHistory{
+		entries: make([]string, 0, limit),
+		limit:   limit,
+	}
+}
+
+func (h *commandHistory) add(command string) {
+	command = strings.TrimSpace(command)
+	if command == "" {
+		return
+	}
+	if len(h.entries) > 0 && h.entries[len(h.entries)-1] == command {
+		return
+	}
+	if len(h.entries) == h.limit {
+		h.entries = h.entries[1:]
+	}
+	h.entries = append(h.entries, command)
 }
 
 func cleanInput(text string) []string {
@@ -70,29 +96,92 @@ func getCommands() map[string]cliCommand {
 	}
 }
 
-func startRepl(cfg *Config) {
-	reader := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		reader.Scan()
+// \r moves cursor to start of line.
+// \033[2K wipes the line
+// print fresh prompt + input
+func redrawInput(prompt string, text []rune) {
+	fmt.Printf("\r\033[2K%s%s", prompt, string(text))
+}
 
-		words := cleanInput(reader.Text())
-		if len(words) == 0 {
-			continue
+func startRepl(cfg *Config) {
+	const prompt = "Pokedex > "
+	history := newCommandHistory(100)
+	historyIndex := 0
+	input := make([]rune, 0, 128)
+
+	err := keyboard.Open()
+	if err != nil {
+		fmt.Println("failed to initialize keyboard input:", err)
+		return
+	}
+	defer keyboard.Close()
+
+	for {
+		redrawInput(prompt, input)
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			fmt.Println("\ninput error:", err)
+			return
 		}
 
-		commandName := words[0]
+		switch key {
+		case keyboard.KeyCtrlC:
+			fmt.Println()
+			return
+		case keyboard.KeyEnter:
+			fmt.Println()
+			line := string(input)
+			input = input[:0]
+			historyIndex = len(history.entries)
 
-		command, exists := getCommands()[commandName]
-		if exists {
-			err := command.callback(cfg, words[1:]...)
-			if err != nil {
-				fmt.Println(err)
+			words := cleanInput(line)
+			if len(words) == 0 {
+				continue
 			}
-			continue
-		} else {
+
+			history.add(line)
+			historyIndex = len(history.entries)
+
+			commandName := words[0]
+			command, exists := getCommands()[commandName]
+			if exists {
+				err := command.callback(cfg, words[1:]...)
+				if err != nil {
+					fmt.Println(err)
+				}
+				continue
+			}
+
 			fmt.Println("Unknown command")
 			continue
+		case keyboard.KeyBackspace, keyboard.KeyBackspace2:
+			if len(input) > 0 {
+				input = input[:len(input)-1]
+			}
+		case keyboard.KeyArrowUp:
+			if len(history.entries) == 0 {
+				continue
+			}
+			if historyIndex > 0 {
+				historyIndex--
+			}
+			input = []rune(history.entries[historyIndex])
+		case keyboard.KeyArrowDown:
+			if len(history.entries) == 0 {
+				continue
+			}
+			if historyIndex < len(history.entries)-1 {
+				historyIndex++
+				input = []rune(history.entries[historyIndex])
+			} else {
+				historyIndex = len(history.entries)
+				input = input[:0]
+			}
+		default:
+			if key == 0 && char != 0 {
+				input = append(input, char)
+				historyIndex = len(history.entries)
+			}
 		}
 	}
 }
